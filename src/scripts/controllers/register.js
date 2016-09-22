493d73e3537106af34d70192de1d02c7ff26b8b8
Saad Shahd <dev.saad.shahd@gmail.com>
2016-09-22 02:04:51 +0200

    Add auth

---
+++ src/scripts/controllers/register.js
@@ -0,0 +1,47 @@
+(function () {
+  angular
+    .module('app')
+    .controller('RegisterCtrl', RegisterCtrl);
+
+  function RegisterCtrl($auth, $state) {
+    const vm = this;
+
+    vm.form = {};
+    vm.submit = submit;
+    vm.github = github;
+
+    function auth(method, ...params) {
+      start();
+
+      $auth[method](...params)
+        .then(success)
+        .catch(fail)
+        .finally(end);
+    }
+
+    function submit() {
+      auth('submitRegistration', vm.form);
+    }
+
+    function github() {
+      auth('authenticate', 'github');
+    }
+
+    function start() {
+      vm.loading = true;
+    }
+
+    function end() {
+      vm.loading = false;
+    }
+
+    function success() {
+      $state.go('dashboard');
+    }
+
+    function fail(err) {
+      const errors = err.errors || err.data.errors;
+      vm.errors = errors;
+    }
+  }
+})();
---
+++ src/scripts/services/auth.js
@@ -0,0 +1,41 @@
+(function () {
+  angular
+    .module('app')
+    .service('authService', authService);
+
+  function authService($q, $auth, $state) {
+    return {
+      redirectAuthed,
+      redirectNotAuthed
+    };
+
+    function redirectMethod(state) {
+      if (state) $state.go(state);
+    }
+
+    function redirectAuth({authedState, notAuthedState}) {
+      function success(res) {
+        redirectMethod(authedState);
+        return res;
+      }
+
+      function fail(err) {
+        redirectMethod(notAuthedState);
+        return err;
+      }
+
+      return $auth
+        .validateUser()
+          .then(success)
+          .catch(fail);
+    }
+
+    function redirectAuthed(toState) {
+      return redirectAuth({authedState: toState});
+    }
+
+    function redirectNotAuthed(toState) {
+      return redirectAuth({notAuthedState: toState});
+    }
+  }
+})();
