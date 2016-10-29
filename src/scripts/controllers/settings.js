3d31d6bf42062d9895d2e4d27d471349558165ee
Islam Wazery <wazery@ubuntu.com>
2016-10-29 11:48:44 +0200

    Fix linting issues

--- src/scripts/controllers/settings.js
+++ src/scripts/controllers/settings.js
@@ -6,8 +6,8 @@
   function SettingsCtrl() {
     var vm = this;
     vm.activePageName = "profile";
-    vm.roles = [{id: 1, name: 'Admin'}, {id: 2, name: 'Admin'}];
-    vm.memberRole = '';
+    vm.roles = [{id: 1, name: "Admin"}, {id: 2, name: "Admin"}];
+    vm.memberRole = "";
     vm.switchPage = function(pageName) {
       vm.activePageName = pageName;
     };
