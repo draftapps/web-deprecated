e84d70fa2ce3cc2d3a81af05935f227168b81e19
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2016-10-30 19:17:53 +0200

    fix for scrolling in settings/profile.html

--- src/scripts/controllers/settings.js
+++ src/scripts/controllers/settings.js
@@ -6,7 +6,7 @@
   function SettingsCtrl() {
     var vm = this;
     vm.activePageName = "profile";
-    vm.roles = [{id: 1, name: 'Admin'}, {id: 2, name: 'Admin'}];
+    vm.roles = [{id: 1, name: "Admin"}, {id: 2, name: "Admin"}];
     vm.memberRole = '';
     vm.switchPage = function(pageName) {
       vm.activePageName = pageName;
