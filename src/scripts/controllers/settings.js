168beb6e8c3cca4886c8b221ba629eda4c91bce3
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2016-11-01 09:30:07 +0200

    Fix dropdown color, placeholder color, and spacing around selected word

--- src/scripts/controllers/settings.js
+++ src/scripts/controllers/settings.js
@@ -7,7 +7,7 @@
     var vm = this;
     vm.activePageName = "profile";
     vm.roles = [{id: 1, name: "Admin"}, {id: 2, name: "Member"}];
-    vm.selectedRole = {};
+    vm.role = {};
     vm.memberRole = "";
     vm.switchPage = function(pageName) {
       vm.activePageName = pageName;
