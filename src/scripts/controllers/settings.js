7f59fbd0a1a63ab9c2ff643c9edeb714f53751df
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2016-11-01 19:08:36 +0200

    using dropdown toggle

--- src/scripts/controllers/settings.js
+++ src/scripts/controllers/settings.js
@@ -8,9 +8,13 @@
     vm.activePageName = "profile";
     vm.roles = [{id: 1, name: "Admin"}, {id: 2, name: "Member"}];
     vm.role = {};
+
     vm.memberRole = "";
     vm.switchPage = function(pageName) {
       vm.activePageName = pageName;
     };
+    vm.setSelectedRole = function (role) {
+      vm.role = role;
+    }
   }
 })();
