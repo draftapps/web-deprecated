8b039737c86f4b0aeb165896e3296294a909deeb
Ahmad Alfy <ahmadalfy@gmail.com>
2016-11-30 02:14:29 +0200

    Add listing of styleguide colors

--- src/scripts/controllers/styleguide.js
+++ src/scripts/controllers/styleguide.js
@@ -3,7 +3,19 @@
     .module("app")
     .controller("StyleguideCtrl", StyleguideCtrl);
 
-  function StyleguideCtrl($scope, $stateParams) {
+  function StyleguideCtrl($scope, $stateParams, projectService, CacheFactory, ENV) {
+
+    var projectCache,
+        projectCacheKey = ENV.api + 'projects/' + $stateParams.id + '?project[slug]=' + $stateParams.slug;
+
+    if (!CacheFactory.get('projectCache')) {
+      CacheFactory.createCache('projectCache', {
+        deleteOnExpire: 'aggressive',
+        recycleFreq: 60000
+      });
+    }
+    projectCache = CacheFactory.get('projectCache');
+
     var vm = this;
     var info;
 
@@ -13,8 +25,18 @@
       id: $stateParams.id,
       slug: $stateParams.slug,
       artboardId: $stateParams.artboardId
-    };
+    }
 
-    hljs.initLineNumbersOnLoad();
+    var info = {
+      id: $stateParams.id,
+      slug: $stateParams.slug
+    }
+    projectService.getProject(info.id, info.slug)
+      .then(function(p) {
+        vm.project = p;
+        hljs.initLineNumbersOnLoad();
+      }, function() {
+        // console.log("Server did not send project data!");
+      });
   }
 })();
