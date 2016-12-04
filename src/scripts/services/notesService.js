a0ae251574236f4740f18e8a3681567ef03dd7ff
Ahmad Alfy <ahmadalfy@gmail.com>
2016-12-05 00:45:09 +0200

    Remove caching from notes

--- src/scripts/services/notesService.js
+++ src/scripts/services/notesService.js
@@ -3,23 +3,12 @@
     .module("app")
     .service("notesService", notesService);
 
-  function notesService($q, $http, CacheFactory, ENV) {
-
-    var notesCache;
-
-    if (!CacheFactory.get('notesCache')) {
-      CacheFactory.createCache('notesCache', {
-        deleteOnExpire: 'aggressive',
-        recycleFreq: 60000
-      });
-    }
-
-    notesCache = CacheFactory.get('notesCache');
+  function notesService($q, $http, ENV) {
 
     return {
       getNotes: function(id, artboard_id) {
         return $q(function(resolve, reject) {
-          $http.get(ENV.api + "projects/" + id + "/artboards/" + artboard_id + "/notes", { cache: notesCache })
+          $http.get(ENV.api + "projects/" + id + "/artboards/" + artboard_id + "/notes")
           .success(function(data) {
             resolve(data);
           }).error(function(data) {
