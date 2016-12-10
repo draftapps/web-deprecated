4d078a9f570138c3043ae34e2af9a802a516b2cb
Ahmad Alfy <ahmadalfy@gmail.com>
2016-12-11 01:49:06 +0200

    Add update team

--- src/scripts/controllers/settings.js
+++ src/scripts/controllers/settings.js
@@ -3,7 +3,7 @@
       .module("app")
       .controller("SettingsCtrl", SettingsCtrl);
 
-  function SettingsCtrl($scope, $stateParams, toastrConfig, toastr, projectService, settingsService) {
+  function SettingsCtrl($scope, $stateParams, $modal, toastrConfig, toastr, projectService, settingsService) {
 
     angular.extend(toastrConfig, {
       target: '.canvas-screen-viewer'
@@ -61,12 +61,21 @@
       );
     };
 
-    projectService.getProjects()
-    .then(function(projects) {
-      $scope.projects = projects;
-    }, function() {
-      // console.log('Server did not send project data!');
-    });
+    var init = function() {
+      projectService.getProjects()
+      .then(function(projects) {
+        $scope.projects = projects;
+        $scope.projectsAdmin = [];
+        _.each(projects, function(project){
+          var member = _.findWhere(project.team.users, {email: $scope.$parent.user.email, role: 1});
+          if(member) {
+            $scope.projectsAdmin.push(project);
+          }
+        });
+      }, function() {
+        // console.log('Server did not send project data!');
+      });
+    }
 
     $scope.inviteMembers = function() {
       // TODO: Should find a cleaner way to do this
@@ -99,6 +108,62 @@
       });
     }
 
+    $scope.projectToManage = "";
+    $scope.setSelectedProject = function() {
+      // TODO: Should find a cleaner way to do this
+      var $project = $('#project-management')
+      $scope.selectedProject = _.findWhere($scope.projects, {slug: $project.val()})
+    }
+
+    $scope.openModal = function(template, parameters) {
+      $scope.projectData = parameters;
+      var params = {
+        templateUrl: template,
+        controller: ["$scope", "$modalInstance", function($scope, $modalInstance) {
+          $scope.reposition = function() {
+            $modalInstance.reposition();
+          };
+          $scope.ok = function() {
+            $modalInstance.close();
+          };
+          $scope.cancel = function() {
+            $modalInstance.dismiss("cancel");
+          };
+        }],
+        scope: $scope
+      };
+      var modalInstance = $modal.open(params);
+      $scope.modal = modalInstance;
+      modalInstance.result.then(function() {
+      }, function() {
+        // Callback when the modal is dismissed.
+        $scope.projectData = {};
+      });
+    };
+
+    $scope.removeMember = function(email) {
+      if($scope.$parent.user.email === email) {
+        $scope.modal.close();
+        toastr.error('You cannot remove yourself from the project');
+        return;
+      }
+      var project = {
+        "project" : {
+          "slug": $scope.selectedProject.slug,
+          "email" : email
+        }
+      };
+      projectService.removeTeamMember($scope.selectedProject.id, project)
+      .then(function(data) {
+        toastr.success('Member removed from your team successfully');
+        $scope.modal.close();
+        $scope.selectedProject = data;
+      }, function(data) {
+        $scope.modal.close();
+        // console.log('Server did not send project data!');
+      });
+    }
+
     function start() {
       vm.loading = true;
     }
@@ -106,5 +171,7 @@
     function end() {
       vm.loading = false;
     }
+
+    init();
   }
 })();
