fcc7598e3b4fea77693486af246f91cad04f000c
Ahmad Alfy <ahmadalfy@gmail.com>
2016-12-13 01:24:21 +0200

    Add new activity type + posting

--- src/scripts/controllers/activity.js
+++ src/scripts/controllers/activity.js
@@ -8,13 +8,27 @@
     var vm = this;
     $scope.menu = "projects-activities";
     $scope.page = "activity";
+    $scope.message = "";
 
     projectService.getProjectActivities($stateParams.id)
+    .then(function(activities) {
+      vm.activity = activities;
+    }, function() {
+      // console.log('Server did not send project data!');
+    });
+
+    $scope.postActivity = function () {
+      var params = {
+        "project_id": $stateParams.id,
+        "message": $scope.message,
+        "user_id": $scope.$parent.user.id
+      }
+      projectService.postProjectActivity($stateParams.id, params)
       .then(function(activities) {
         vm.activity = activities;
       }, function() {
         // console.log('Server did not send project data!');
       });
-
+    }
   }
 })();
